79f1a2bf053c0bde3b682714b30aa073d7b2a2c2
kyungjoongo77777 <127845147+kyungjoongo77777@users.noreply.github.com>
2023-07-21 07:25:10 +0900

    updated rest request

--- src/main/kotlin/com/kyungjoon/interceptors/InterceptorOne.java
+++ src/main/kotlin/com/kyungjoon/interceptors/InterceptorOne.java
@@ -1,6 +1,7 @@
 package com.kyungjoon.interceptors;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -18,22 +19,14 @@ public class InterceptorOne implements HandlerInterceptor {
         if (authorizationHeaderValue != null && authorizationHeaderValue.startsWith("Bearer")) {
             String token = authorizationHeaderValue.substring(7, authorizationHeaderValue.length());
             System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-            System.out.println("token===>" + token);
-
             return true;
         } else {//todo : 해더가 없는 경우 인증이 안된 api호출 이므로 return false.!
 //            ObjectMapper mapper = new ObjectMapper();
 //            res.setContentType("application/json");
 //            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 //            res.getWriter().write(mapper.writeValueAsString("no auth"));
-            System.out.println("preHandle!!!");
+//            System.out.println("preHandle!!!");
+//            return false;
             return true;
         }
 
